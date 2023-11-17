4204aa56d61e0532cdc9865681ffbf55ff7db74a
sivachandrika <sivachandrika66@gmail.com>
2023-11-17 12:52:48 +0530

    added files

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sivachandrika/simple-plagiarism-checker-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sivachandrika/simple-plagiarism-checker-app
